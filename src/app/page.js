2619400d0a675cb5f41fcf426bf42c1912b4b4e9
Alan <alandouglas96@gmail.com>
2023-11-09 11:37:53 +0100

    Add optimisations

--- src/app/page.js
+++ src/app/page.js
@@ -12,14 +12,34 @@ const IMAGE_URL = 'https://alandouglasphotography.s3.eu-central-1.amazonaws.com'
 const AUDIO_URL =
   'https://alandouglasphotography.s3.eu-central-1.amazonaws.com/sir_duke.mp3'
 
+const IMAGE_SRC = {
+  LAYER_1:
+    'https://cdn.sanity.io/images/7og2dskj/production/44dabe5455dda333e31c6956435a0f8b912f3113-1920x1080.png',
+  LAYER_2:
+    'https://cdn.sanity.io/images/7og2dskj/production/cc6ec055ddd8036e545e468ac5e31fb1926145e5-1920x1080.png',
+  LAYER_3:
+    'https://cdn.sanity.io/images/7og2dskj/production/5b1345c219fe0267cadbbe5f059e4b5786d5bc6c-1920x1080.png',
+  LAYER_4:
+    'https://cdn.sanity.io/images/7og2dskj/production/47113b75e44c69170f16afe9ebe5cbe5a6c5f98b-1920x1080.png',
+  LAYER_5:
+    'https://cdn.sanity.io/images/7og2dskj/production/79819fb6136056ebcffa47390dc542170c0be73e-1920x1080.png',
+  LAYER_6:
+    'https://cdn.sanity.io/images/7og2dskj/production/9b24070235e6dc58cbf3b646cf79a2d41be229a6-1920x1080.png',
+  NAME: 'https://cdn.sanity.io/images/7og2dskj/production/cd30abebf5f45ada63211c5f97777cbc8a7e1615-1920x1080.png',
+  TITLE:
+    'https://cdn.sanity.io/images/7og2dskj/production/30cb663575c72a804d4a1a459de362b0d0d56789-1920x1080.png',
+}
+
 const useAudio = (url) => {
   const [playing, setPlaying] = useState(false)
 
   const [audio, setAudio] = useState(null)
 
   useEffect(() => {
-    setAudio(new Audio(url))
-    // only run once on the first render on the client
+    // iframe detection
+    if (window.location == window.parent.location) {
+      setAudio(new Audio(url))
+    }
   }, [])
 
   const toggle = () => setPlaying(!playing)
@@ -40,6 +60,7 @@ const useAudio = (url) => {
 export default function Home() {
   const anchorRef = useRef()
 
+  // iframe detection
   const [isIframe, setIsIframe] = useState(false)
 
   const [position, setPosition] = useState({ x: 0, y: 0 })
@@ -61,11 +82,6 @@ export default function Home() {
     return () => window.removeEventListener('mousemove', handleMove)
   }, [])
 
-  // useEffect(() => {
-  //   window.addEventListener('click', toggle)
-  //   return () => window.removeEventListener('click', toggle)
-  // })
-
   return (
     <main className={styles.main}>
       {!playing ? (
@@ -86,7 +102,8 @@ export default function Home() {
         }}
       />
       <Image
-        src={`${IMAGE_URL}/layer_1.png`}
+        // src={`${IMAGE_URL}/layer_1.png`}
+        src={IMAGE_SRC.LAYER_1}
         quality={100}
         fill={true}
         style={{
@@ -96,7 +113,8 @@ export default function Home() {
         alt="Layer 1"
       />
       <Image
-        src={`${IMAGE_URL}/layer_2.png`}
+        // src={`${IMAGE_URL}/layer_2.png`}
+        src={IMAGE_SRC.LAYER_2}
         quality={100}
         fill={true}
         style={{
@@ -110,7 +128,8 @@ export default function Home() {
         className={isIframe ? styles.layer2 : ''}
       />
       <Image
-        src={`${IMAGE_URL}/stevie_name.png`}
+        // src={`${IMAGE_URL}/stevie_name.png`}
+        src={IMAGE_SRC.NAME}
         quality={100}
         fill={true}
         style={{
@@ -125,7 +144,8 @@ export default function Home() {
         className={isIframe ? styles.name : ''}
       />
       <Image
-        src={`${IMAGE_URL}/layer_3.png`}
+        // src={`${IMAGE_URL}/layer_3.png`}
+        src={IMAGE_SRC.LAYER_3}
         quality={100}
         fill={true}
         style={{
@@ -139,7 +159,8 @@ export default function Home() {
         className={isIframe ? styles.layer3 : ''}
       />
       <Image
-        src={`${IMAGE_URL}/stevie_title.png`}
+        // src={`${IMAGE_URL}/stevie_title.png`}
+        src={IMAGE_SRC.TITLE}
         quality={100}
         fill={true}
         style={{
@@ -154,7 +175,8 @@ export default function Home() {
         className={isIframe ? styles.title : ''}
       />
       <Image
-        src={`${IMAGE_URL}/layer_4.png`}
+        // src={`${IMAGE_URL}/layer_4.png`}
+        src={IMAGE_SRC.LAYER_4}
         quality={100}
         fill={true}
         style={{
@@ -168,7 +190,8 @@ export default function Home() {
         className={isIframe ? styles.layer4 : ''}
       />
       <Image
-        src={`${IMAGE_URL}/layer_5.png`}
+        // src={`${IMAGE_URL}/layer_5.png`}
+        src={IMAGE_SRC.LAYER_5}
         quality={100}
         fill={true}
         style={{
@@ -182,7 +205,8 @@ export default function Home() {
         className={isIframe ? styles.layer5 : ''}
       />
       <Image
-        src={`${IMAGE_URL}/layer_6.png`}
+        // src={`${IMAGE_URL}/layer_6.png`}
+        src={IMAGE_SRC.LAYER_6}
         quality={100}
         fill={true}
         style={{
